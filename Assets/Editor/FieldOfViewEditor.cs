a183fc62c484d9f56c4e274a55ad46924f415919
Wayne <wayne2k@gmail.com>
2016-01-12 20:25:09 +0200

    Part 1

---
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+	void OnSceneGUI ()
+	{
+		FieldOfView fov = target as FieldOfView;
+
+		Handles.color = Color.white;
+		Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+
+		Vector3 viewAngleA = fov.DirectionFromAngle(-fov.viewAngle/2, false);
+		Vector3 viewAngleB = fov.DirectionFromAngle(fov.viewAngle/2, false);
+
+		Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+		Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+
+		Handles.color = Color.red;
+		foreach (var visibleTarget in fov.visibleTargets) {
+			Handles.DrawLine(fov.transform.position, visibleTarget.position);
+		}
+	}
+}
---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FieldOfView : MonoBehaviour 
+{
+	public float viewRadius;
+	[Range (0, 360)]
+	public float viewAngle;
+
+	public LayerMask targetMask;
+	public LayerMask obstacleMask;
+
+	public List<Transform> visibleTargets = new List<Transform>();
+
+	void Start ()
+	{
+		StartCoroutine(FindTargetsWithDelay(.2f));
+	}
+
+	IEnumerator FindTargetsWithDelay (float delay)
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(delay);
+			FindVisibleTargets();
+		}
+	}
+
+	void FindVisibleTargets ()
+	{
+		visibleTargets.Clear();
+		Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+		for (int i = 0; i < targetsInViewRadius.Length; i++) 
+		{
+			Transform target = targetsInViewRadius[i].transform;
+
+			Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+			if (Vector3.Angle(transform.forward, directionToTarget) < viewAngle / 2)
+			{
+				float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+				if (Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask) == false)
+				{
+					visibleTargets.Add(target);
+				}
+			}
+		}
+	}
+
+	public Vector3 DirectionFromAngle (float angleInDegrees, bool angleIsGlobal)
+	{
+		if (angleIsGlobal == false) {
+			angleInDegrees += transform.eulerAngles.y;
+		}
+
+		return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0f, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+	}
+
+
+}
